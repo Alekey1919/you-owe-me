268aa3164bd023dbea6cc680bd4734d86b715f1f
Alejo Espindola <alejosa1919@gmail.com>
2024-10-25 22:43:21 -0300

    Expense per user result responsiveness

--- src/app/split_ticket/[[...page]]/UserBalance.tsx
+++ src/app/split_ticket/[[...page]]/UserBalance.tsx
@@ -1,6 +1,11 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { IUserBalance, IUserBalanceAmountsDetails } from "../../types/types";
 import { twMerge } from "tailwind-merge";
+import Image from "next/image";
+import Arrow from "@public/images/back-arrow.svg";
+import useMediaQueryState, {
+  DefaultBreakpoints,
+} from "@/app/hooks/useMediaQueryState";
 
 const DetailsSection = ({
   text,
@@ -14,8 +19,8 @@ const DetailsSection = ({
   isInFavor?: boolean;
 }) => {
   return (
-    <div className="flex flex-col space-y-2 border-background border-solid border-[2px] p-2 rounded-xl">
-      <span>
+    <div className="flex flex-col space-y-2 border-background border-solid border-[2px] p-2 rounded-xl text-base 2xl:text-lg">
+      <span className="underline">
         {text}{" "}
         <span
           className={twMerge(
@@ -58,27 +63,56 @@ const UserBalance = ({
   userName: string;
   userBalance: IUserBalance;
 }) => {
+  const smScreen = useMediaQueryState({ breakpoint: DefaultBreakpoints.sm });
+  const [isOpen, setIsOpen] = useState(false);
+
   const owesMoney = userBalance.balance < 0;
 
-  return (
-    <div className="box flex flex-col space-y-2">
-      <span className="text-lg font-medium pl-2">{userName}</span>
+  useEffect(() => {
+    setIsOpen(smScreen);
+  }, [smScreen]);
 
-      <div className="flex flex-col space-y-2">
-        <DetailsSection
-          text="Total amount to pay:"
-          totalAmount={userBalance.amountToPay.total}
-          expenses={userBalance.amountToPay.details}
-        />
-        <DetailsSection
-          text="Total amount payed:"
-          totalAmount={userBalance.amountPayed.total}
-          expenses={userBalance.amountPayed.details}
-          isInFavor
+  return (
+    <div className={twMerge("box flex flex-col relative ", isOpen && "!pb-16")}>
+      <div className="flex justify-between">
+        <span className="text-lg font-medium pl-2">{userName}</span>
+        <Image
+          src={Arrow}
+          alt={isOpen ? "Close" : "Open"}
+          className={twMerge(
+            "w-6 transition-transform ",
+            isOpen ? "rotate-90" : "rotate-[-90deg]"
+          )}
+          onClick={() => setIsOpen((curr) => !curr)}
         />
       </div>
 
-      <span className="pl-2">
+      <div
+        className={twMerge(
+          "transition-height !transition-all !duration-300",
+          isOpen && "open mt-2"
+        )}
+      >
+        <div className="flex flex-col space-y-2">
+          <DetailsSection
+            text="Total amount to pay:"
+            totalAmount={userBalance.amountToPay.total}
+            expenses={userBalance.amountToPay.details}
+          />
+          <DetailsSection
+            text="Total amount payed:"
+            totalAmount={userBalance.amountPayed.total}
+            expenses={userBalance.amountPayed.details}
+            isInFavor
+          />
+        </div>
+      </div>
+      <span
+        className={twMerge(
+          "pl-2 text-base 2xl:text-lg absolute bottom-4 opacity-0 transition-opacity",
+          isOpen && "opacity-100"
+        )}
+      >
         Balance:{" "}
         <span
           className={twMerge(owesMoney ? "text-red-400" : "text-green-400")}
--- src/app/split_ticket/[[...page]]/page.tsx
+++ src/app/split_ticket/[[...page]]/page.tsx
@@ -24,11 +24,11 @@ const Results = () => {
   }, [transactions]);
 
   return (
-    <div className="min-h-screen min-w-[100vh] flex flex-col items-center space-y-20 pt-20">
-      <div className="flex flex-col w-full justify-center space-y-20">
-        <div className="flex flex-col space-y-10">
-          <h1 className="text-2xl">Expenses per user</h1>
-          <div className="grid grid-cols-4 px-10 gap-4">
+    <div className="w-full flex flex-col items-center space-y-20 py-8 3xl:py-14 px-8 3xl:px-10">
+      <div className="flex flex-col w-full justify-center space-y-10 3xl:space-y-20">
+        <div className="flex flex-col space-y-6 3xl:space-y-10">
+          <h1 className="text-2xl 2xl:text-3xl">Expenses per user</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
             {Object.keys(userBalances).map((participant, index) => {
               return (
                 <UserBalance
@@ -40,10 +40,10 @@ const Results = () => {
             })}
           </div>
         </div>
-        <div className="flex flex-col space-y-10">
+        <div className="flex flex-col space-y-6 3xl:space-y-10">
           <h1 className="text-2xl">Transactions</h1>
 
-          <div className="flex space-x-4">
+          <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4">
             {Object.keys(groupedTransactions).map((payer, index) => {
               return (
                 <UserTransactions
