e937a2005cc9dd6af909e73b5e2563e065cae142
Alejo Espindola <alejosa1919@gmail.com>
2024-11-09 00:01:33 -0300

    Log the variable directly

--- src/app/api/debug/route.js
+++ src/app/api/debug/route.js
@@ -1,11 +1,8 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  // Log the Firebase API Key or any other environment variable
-  console.log("FIREBASE_API_KEY:", process.env.FIREBASE_API_KEY);
-
-  // Respond with a success message (but do not expose any sensitive data in the response)
   return NextResponse.json({
+    firebaseApiKey: process.env.FIREBASE_API_KEY || "Not Loaded",
     message: "Debug information logged successfully",
   });
 }
