5e1463701aae4d418ede533b12e5a69f96cca281
Alejo Espindola <alejosa1919@gmail.com>
2024-11-09 00:05:14 -0300

    Using NEXT_PUBLIC

--- src/app/api/debug/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({
-    firebaseApiKey: process.env.FIREBASE_API_KEY || "Not Loaded",
-    message: "Debug information logged successfully",
-  });
-}
