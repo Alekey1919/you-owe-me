a1fa30bfdc9a6c4ca2f12beda7ee59386a796faa
Alejo Espindola <alejosa1919@gmail.com>
2024-11-08 22:48:44 -0300

    Fixed build

--- src/app/my_tickets/page.tsx
+++ src/app/my_tickets/page.tsx
@@ -13,7 +13,7 @@ import { selectUser } from "../redux/slices/userSlice";
 import { getUserTickets } from "../lib/fetchData";
 import { DocumentData, QueryDocumentSnapshot } from "firebase/firestore";
 
-export const PAGE_SIZE = 10;
+const PAGE_SIZE = 10;
 
 const Page = () => {
   const [tickets, setTickets] = useState<ITicket[]>([]);
